d9f30c5276b0b74bb0720b21776927c1b357bfb5
Yurii <yurii.androsiuk@gmail.com>
2020-08-19 17:41:50 +0300

    databind

--- src/mock/task.js
+++ src/mock/task.js
@@ -5,6 +5,8 @@ import {
   getRandomInteger
 } from "../utils/common.js";
 
+const generateId = () => Date.now() + parseInt(Math.random() * 10000, 10);
+
 const generateDescription = () => {
   const descriptions = [
     `Изучить теорию`,
@@ -67,6 +69,7 @@ export const generateTask = () => {
     };
 
   return {
+    id: generateId(),
     description: generateDescription(),
     dueDate,
     repeating,
